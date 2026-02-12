f28fd397a4b4e3fa720bc232277f501472c72fe1
DevTimmy <oyinloyematthew6@gmail.com>
2026-02-12 22:45:32 +0100

    Prod: Adding production API

--- src/server.js
+++ src/server.js
@@ -23,10 +23,18 @@ const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 // Middleware
+// app.use(cors({
+//   origin: FRONTEND_URL,
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: FRONTEND_URL,
-  credentials: true,
-}));
+  origin: [
+    'http://localhost:3000',
+    FRONTEND_URL
+  ],
+  credentials: true
+}))
 app.use(express.json());
 
 // Store io instance globally
